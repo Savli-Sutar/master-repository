349b189cee144f267648c25d9938a599629cec1c
Savli-Sutar <its.me.savli.03@gmail.com>
2026-01-28 21:11:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,50 @@ function fillBin(binNo) {
         progressBar.style.width = "0%";
     }
 }
+const URL = "https://teachablemachine.withgoogle.com/models/qzbWj1b7y/";
+let model, webcam, labelContainer, maxPredictions;
+
+async function init() {
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
+
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    const flip = true; 
+    webcam = new tmImage.Webcam(300, 300, flip); 
+    await webcam.setup(); 
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    labelContainer = document.getElementById("label-container");
+    for (let i = 0; i < maxPredictions; i++) {
+        labelContainer.appendChild(document.createElement("div"));
+    }
+}
+
+async function loop() {
+    webcam.update();
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+async function predict() {
+    const prediction = await model.predict(webcam.canvas);
+    
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction = prediction[i].className + ": " + (prediction[i].probability * 100).toFixed(0) + "%";
+        labelContainer.childNodes[i].innerHTML = classPrediction;
+
+        // AUTOMATION: If AI sees "Plastic" or "Organic" with 95% confidence, it fills the bin!
+        if (prediction[i].probability > 0.95) {
+            if (prediction[i].className === "Plastic") {
+                fillBin(1); // Trigger Bin 1
+            } else if (prediction[i].className === "Organic") {
+                fillBin(2); // Trigger Bin 2
+            }
+        }
+    }
+}
+
